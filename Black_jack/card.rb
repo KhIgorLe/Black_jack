a2aab88f68878a64d703470fdf16f6833eece22b
Rybu system <KhIgorLe>
2018-12-10 20:18:28 +0200

    Fixed again

--- Black_jack/card.rb
+++ Black_jack/card.rb
@@ -2,16 +2,6 @@ class Card
   RANKS = %w(2 3 4 5 6 7 8 9 10 J Q K A).freeze
   SUITS = %w(♥ ♦ ♣ ♠).freeze
 
-  class << self
-    def ranks
-      RANKS
-    end
-
-    def suits
-      SUITS
-    end
-  end
-
   attr_reader :rank, :suit
 
   def initialize(rank, suit)
--- Black_jack/deck.rb
+++ Black_jack/deck.rb
@@ -15,8 +15,8 @@ class Deck
 
   def set_cards
     cards = []
-    Card.ranks.each do |rank|
-      Card.suits.each do |suit|
+    Card::RANKS.each do |rank|
+      Card::SUITS.each do |suit|
         cards << Card.new(rank, suit)
       end
     end
